69e0616650b7237c70cdbb0e8dc39c9eaf43083b
Matheus Lucas <nerdola.cartola@gmail.com>
2025-05-18 02:30:13 -0300

    Add fibonnaci example

--- publisher.py
+++ publisher.py
@@ -1,11 +1,30 @@
 import zmq, time
 from constPS import * #-
 
+import threading
+import time
+
+# Function to compute Fibonacci numbers
+def compute_fibonacci(s):
+    a, b = 0, 1
+    i = 0
+    
+    while True:
+        msg = str.encode(f"FIB ({i}) = {a}")
+        s.send(msg)
+        a, b = b, a + b
+        time.sleep(1.5)  # Just to slow it down for visibility
+        i += 1
+
 context = zmq.Context()
 s = context.socket(zmq.PUB)        # create a publisher socket
 p = "tcp://"+HOST+":"+ PORT      # how and where to communicate
 s.bind(p)                          # bind socket to the address
+fib_thread = threading.Thread(target=compute_fibonacci, args=(s,))
+fib_thread.start()
 while True:
 	time.sleep(5)                    # wait every 5 seconds
 	msg = str.encode("TIME " + time.asctime())
+	s.send(msg) # publish the current time
+	msg = str.encode("NEWS " + "hello")
 	s.send(msg) # publish the current time
--- subscriber.py
+++ subscriber.py
@@ -6,7 +6,9 @@ s = context.socket(zmq.SUB)          # create a subscriber socket
 p = "tcp://"+ HOST +":"+ PORT        # how and where to communicate
 s.connect(p)                         # connect to the server
 s.setsockopt_string(zmq.SUBSCRIBE, "TIME")  # subscribe to TIME messages
+s.setsockopt_string(zmq.SUBSCRIBE, "NEWS")  # subscribe to NEWS messages
+s.setsockopt_string(zmq.SUBSCRIBE, "FIB")  # subscribe to NEWS messages
 
-for i in range(5):  # Five iterations
+while True:  # Five iterations
 	time = s.recv()   # receive a message
 	print (bytes.decode(time))
